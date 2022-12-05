43efde4e76eafb9dbad2f267374e44c89e3559df
bk <builtbybk@gmail.com>
2022-12-05 15:31:21 -0500

    ddf.sh adjustments

--- scripts/discard-dev-files.sh
+++ scripts/discard-dev-files.sh
@@ -6,8 +6,11 @@ rm -rf \{\{cookiecutter.project_slug\}\}/.git
 rm -rf \{\{cookiecutter.project_slug\}\}/backend/app/poetry.lock
 rm -rf \{\{cookiecutter.project_slug\}\}/frontend/node_modules
 rm -rf \{\{cookiecutter.project_slug\}\}/frontend/dist
+rm -rf \{\{cookiecutter.project_slug\}\}/react/node_modules
+rm -rf \{\{cookiecutter.project_slug\}\}/react/dist
 git checkout \{\{cookiecutter.project_slug\}\}/README.md
 git checkout \{\{cookiecutter.project_slug\}\}/.gitlab-ci.yml
 git checkout \{\{cookiecutter.project_slug\}\}/cookiecutter-config-file.yml
 git checkout \{\{cookiecutter.project_slug\}\}/.env
 git checkout \{\{cookiecutter.project_slug\}\}/frontend/.env
+git checkout \{\{cookiecutter.project_slug\}\}/react/.env
